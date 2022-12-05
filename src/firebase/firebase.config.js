fef921c6f88dae12386c1fd37a82eed5617b93be
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-12-05 19:09:17 +0600

    basic setup complete

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtzIzHAy-jVL7VEfxESyYQG8BXOrxNHCY",
+  authDomain: "aircnc-ddc74.firebaseapp.com",
+  projectId: "aircnc-ddc74",
+  storageBucket: "aircnc-ddc74.appspot.com",
+  messagingSenderId: "278240479322",
+  appId: "1:278240479322:web:59f73769326842a9273063",
+  measurementId: "G-L6LL513VR3"
+};
+
+const app = initializeApp(firebaseConfig)
+
+export default app
